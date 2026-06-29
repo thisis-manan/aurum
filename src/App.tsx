a9da7215afd7da975c5dccdbbd8cd5e3c1612103
Rachna Bhandari <rachna.bhandari193@gmail.com>
2026-06-29 21:10:49 +0530

    fix: resolve all merge conflicts in globals.css and App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
+import Nav from './components/Nav/Nav'
+import Hero from './components/Hero/Hero'
+import CategoryGrid from './components/CategoryGrid/CategoryGrid'
 import ProductShowcase from './components/ProductShowcase/ProductShowcase'
 import Carousel from './components/Carousel/Carousel'
+import Footer from './components/Footer/Footer'
+import { useLenis } from './hooks/useLenis'
 
 function App() {
+  useLenis()
   return (
-    <div>
-      <ProductShowcase />
-      <Carousel />
-    </div>
+    <>
+      <Nav />
+      <main>
+        <Hero />
+        <CategoryGrid />
+        <ProductShowcase />
+        <Carousel />
+      </main>
+      <Footer />
+    </>
   )
 }
 
